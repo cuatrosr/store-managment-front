bba33913dc0c9e5168f25bf46252f1c964529c09
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2022-12-01 00:44:52 -0500

    project created

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import './index.css';
+import App from './App';
+import Navbar from './components/Navbar';
+import Register from './components/Register';
+import List from './components/List';
+import Cart from './components/Cart';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Navbar />
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<App/>} />
+        <Route path='register' element={<Register/>} />
+        <Route path='list' element={<List/>} />
+        <Route path='cart' element={<Cart/>} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
